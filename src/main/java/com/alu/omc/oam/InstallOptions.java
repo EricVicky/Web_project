2d40a47e7cca7d3a70e3f999c6b87a445dbc6949
Amos Chu <amos.chu@centling.com>
2015-03-27 15:55:30 +0800

    modify json map

--- src/main/java/com/alu/omc/oam/InstallOptions.java
+++ src/main/java/com/alu/omc/oam/InstallOptions.java
@@ -12,9 +12,22 @@ public class InstallOptions
 {
     private static Map  opMap  = new HashMap();
     private final static String INSTALL_OPTIONS_JSOM_FILE = "install_option_map.json";
-    static
-    {
-        try
+//    static
+//    {
+//        try
+//        {
+//            URI uri = InstallOptions.class.getResource(INSTALL_OPTIONS_JSOM_FILE).toURI();
+//            opMap = Json2Object.toMap(new File(uri));
+//        }
+//        catch (URISyntaxException e)
+//        {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        }
+//    }
+    
+    public static String get(COMType comType, String vm){
+    	try
         {
             URI uri = InstallOptions.class.getResource(INSTALL_OPTIONS_JSOM_FILE).toURI();
             opMap = Json2Object.toMap(new File(uri));
@@ -24,12 +37,13 @@ public class InstallOptions
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-    }
-    
-    public static String get(COMType comType, String vm){
-        Map<String, String> comTypeOptions = (Map)opMap.get(comType);
+        Map<String, String> comTypeOptions = (Map)opMap.get(comType.toString());
         return comTypeOptions.get(vm);
 
+    }
+    
+    public static void main(String[] args) {
+    	InstallOptions.get(COMType.FCAPS, "oam");
     }
 
 }
