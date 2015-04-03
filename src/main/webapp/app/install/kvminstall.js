facfb65fada129991403441074082c7082702590
amos <amos.chu@alcatel-lucent.com>
2015-04-03 15:25:52 +0800

    modify ui

--- src/main/webapp/app/install/kvminstall.js
+++ src/main/webapp/app/install/kvminstall.js
@@ -58,6 +58,7 @@ app.controller('kvmctr', function($scope, $q, $timeout, $log, KVMService, websoc
             $scope.showlog= function(data){
             	$log.info(data);
             	logviewer.append(data.body + "\n");
+                logviewer.css({ display: "block" });
             }
 			$scope.deploy = function (){
             	KVMService.deploy(
