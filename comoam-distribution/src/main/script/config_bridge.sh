8329b174f92c0062dfb3ed9024811b61bd314977
Jeff Hu <Jeff.hu1@alcatel-lucent.com>
2015-05-24 11:40:03 +0800

    config bridge

--- comoam-distribution/src/main/script/config_bridge.sh
+++ comoam-distribution/src/main/script/config_bridge.sh
@@ -3,8 +3,21 @@ export HWADDR=$(cat /etc/sysconfig/network-scripts/ifcfg-eth0 |grep -oP '(?<=HWA
 export IPADDR=$(cat /etc/sysconfig/network-scripts/ifcfg-eth0 |grep -oP '(?<=IPADDR=).*$')
 export NETMASK=$(cat /etc/sysconfig/network-scripts/ifcfg-eth0 |grep -oP '(?<=NETMASK=).*$')
 export GATEWAY=$(cat /etc/sysconfig/network-scripts/ifcfg-eth0 |grep -oP '(?<=GATEWAY=).*$')
-
 echo HWADDR=$HWADDR
 echo IPADDR=$IPADDR
 echo NETMASK=$NETMASK
 echo GATEWAY=$GATEWAY
+# resolve links - $0 may be a softlink
+PRG="$0"
+while [ -h "$PRG" ] ; do
+  ls=`ls -ld "$PRG"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    PRG="$link"
+  else
+    PRG=`dirname "$PRG"`/"$link"
+  fi
+done
+PRGDIR=`dirname "$PRG"`
+VARS="eth=$eth br=$br HWADDR=$HWADDR IPADDR=$IPADDR NETMASK=$NETMASK GATEWAY=$GATEWAY"
+ansible-playbook -i ../comoam-playbook/inventory/hosts.local -e "$VARS"  $PRGDIR/../comoam-playbook/playbooks/kvm/install_bridge.yml
