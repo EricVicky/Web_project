5bca26f8a37042c4a9060bf25b9daff28b0090ad
Jeff Hu <Jeff.hu1@alcatel-lucent.com>
2015-07-10 13:40:59 +0800

    refact ssl key

--- comoam-distribution/src/main/script/postinstall.sh
+++ comoam-distribution/src/main/script/postinstall.sh
@@ -20,44 +20,11 @@ if [ ! -d /opt/PlexView/ELCM/ELCM-playbook/hot_files ]; then
     mkdir /opt/PlexView/ELCM/ELCM-playbook/hot_files 
 fi
 chmod +x /opt/PlexView/ELCM/server/bin/*sh
-#key file generation and import
-apachePKCS12File=/tmp/server.pkcs12
-tomcatHomeDir=/opt/PlexView/ELCM/server
-tomcatKeyStoreDir="$tomcatHomeDir/ssl/keystore"
-if [ ! -f $tomcatKeyStoreDir/elcm.jks ]
-then
-        cd /tmp
-        if [ -f $apachePKCS12File ]; then
-                if [ ! -d $tomcatKeyStoreDir ]; then
-                        mkdir -p $tomcatKeyStoreDir
-                fi
 
-                export RANDFILE=$tomcatKeyStoreDir/.rnd
-                if [ -f "$tomcatKeyStoreDir/*.rnd" ]; then
-                        chmod  755  $tomcatKeyStoreDir/*.rnd
-                fi
+echo "generate ssl key"
+/opt/PlexView/ELCM/script/sslKey.sh
 
-                echo "Convert Apache PKCS12 file to Java PKCS12 key store ..."
-                openssl pkcs12 -in $apachePKCS12File -out server.pem -passin pass:serverpkcs12 -passout pass:serverpkcs12
-                openssl pkcs12 -export -in server.pem -out server.p12 -name "qosacert" -passin pass:serverpkcs12 -passout pass:serverpkcs12
 
-                echo "Convert Java PKCS12 file to a JKS key store ..."
-                keytool -importkeystore -srcstorepass serverpkcs12 -srckeystore server.p12 -destkeystore ELCM.jks -deststorepass serverpkcs12 -srcstoretype pkcs12 -deststoretype JKS
-
-                echo "Remove file server.pem ..."
-                rm -f server.pem echo "Remove file server.p12 ..."
-                rm -f server.p12
-
-                echo "Install Java Keystore file in Tomcat ..."
-                mv -f ELCM.jks $tomcatKeyStoreDir
-
-                echo "Tomcat keystore file elcm.jks has been created successfully!"
-        else
-                echo "Tomcat keystore file will not be created.";
-        fi
-else
-        echo "Tomcat keystore file already exists, will not be created again.";
-fi
 if [ -f /opt/PlexView/ELCM/datasource/comstack.json ]; then
     /opt/PlexView/ELCM/script/migration-0.7-8.py
 fi
