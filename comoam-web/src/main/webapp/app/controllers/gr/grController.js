811cd85d4e36822c114e923c9c874ca905c10ad7
hacui <harvey.cui@alcatel-lucent.com>
2015-07-13 15:36:06 +0800

    gr ip config for openstack

--- comoam-web/src/main/webapp/app/controllers/gr/grController.js
+++ comoam-web/src/main/webapp/app/controllers/gr/grController.js
@@ -40,23 +40,25 @@ angular.module('gr', [ 'ui.router',
     		 GRService.checkInstalled({"name":$scope.gr_config.pri.deployment_prefix}).then(function(data){
     			 $scope.installedPriGR = (data.succeed == true?"Installed":"Not Installed");
         	 });
-    		 $scope.priOAMRowspan = $scope.gr_config.pri.vm_config.oam.nic.length * 2;
- 	    	 $scope.priDBRowspan = $scope.gr_config.pri.vm_config.db.nic.length * 2;
- 	    	 if($scope.gr_config.pri.comType != "OAM"){
- 	    		 $scope.priCMRowspan = $scope.gr_config.pri.vm_config.cm.nic.length * 2; 	    		 
- 	    	 }
- 	      	 
+    		 if($scope.gr_config.pri.environment == "KVM"){
+    			 $scope.priOAMRowspan = $scope.gr_config.pri.vm_config.oam.nic.length * 2;
+    			 $scope.priDBRowspan = $scope.gr_config.pri.vm_config.db.nic.length * 2;
+    			 if($scope.gr_config.pri.comType != "OAM"){
+    				 $scope.priCMRowspan = $scope.gr_config.pri.vm_config.cm.nic.length * 2; 	    		 
+    			 }
+    		 }
     	 };
     	 $scope.secondarycfg = function(){
     		 GRService.checkInstalled({"name":$scope.gr_config.sec.deployment_prefix}).then(function(data){
     			 $scope.installedSecGR = (data.succeed == true?"Installed":"Not Installed");
         	 });
-    		 $scope.secOAMRowspan = $scope.gr_config.sec.vm_config.oam.nic.length * 2;
- 	    	 $scope.secDBRowspan = $scope.gr_config.sec.vm_config.db.nic.length * 2;
- 	    	 if($scope.gr_config.sec.comType != "OAM"){
- 	    		 $scope.secCMRowspan = $scope.gr_config.sec.vm_config.cm.nic.length * 2; 	    		 
- 	    	 }
- 	      	 
+    		 if($scope.gr_config.sec.environment == "KVM"){
+    			 $scope.secOAMRowspan = $scope.gr_config.sec.vm_config.oam.nic.length * 2;
+    			 $scope.secDBRowspan = $scope.gr_config.sec.vm_config.db.nic.length * 2;
+    			 if($scope.gr_config.sec.comType != "OAM"){
+    				 $scope.secCMRowspan = $scope.gr_config.sec.vm_config.cm.nic.length * 2; 	    		 
+    			 }
+    		 }
     	 };
     	 
     	 $scope.gr_IP = {"pri":{}, "sec": {}};
@@ -66,49 +68,62 @@ angular.module('gr', [ 'ui.router',
     		 for(var vm in vm_config){
     			 //single vm comfig
     			 var cfg = vm_config[vm];
-    			 for(var i =0; i <= IPNum; i++){
-    				 if(i==0){
-    					 $scope.gr_IP[role][vm] = [];
-    				 }
-    				 if($scope.gr_config.gr_ip_type == 'ipv4'){
-    					 $scope.gr_IP[role][vm].push(cfg.nic[i].ip_v4.ipaddress);
-    				 }else{
-    					 if(cfg.nic[i].ip_v6){
-    						 $scope.gr_IP[role][vm].push(cfg.nic[i].ip_v6.ipaddress);
+    			 if($scope.gr_config[role].environment == "KVM"){
+    				 for(var i =0; i <= IPNum; i++){
+    					 if(i==0){
+    						 $scope.gr_IP[role][vm] = [];
+    					 }
+    					 if($scope.gr_config.gr_ip_type == 'ipv4'){
+    						 $scope.gr_IP[role][vm].push(cfg.nic[i].ip_v4.ipaddress);
+    					 }else{
+    						 if(cfg.nic[i].ip_v6){
+    							 $scope.gr_IP[role][vm].push(cfg.nic[i].ip_v6.ipaddress);
+    						 }
     					 }
     				 }
+    			 }else{
+    				 $scope.gr_IP[role][vm] = [];
+    				 $scope.gr_IP[role][vm].push(cfg.provider_ip_address);
     			 }
     		 }
     		 
     	 };
     	 $scope.gr_ip_changed = function(){
-    		 if($scope.gr_config.gr_traffic && $scope.gr_config.gr_ip_type){
-    			 $scope.gr_ip_setup('pri');
-    			 $scope.gr_ip_setup('sec');
-    		 }
-    		 $log.info($scope.gr_IP);
-    		 
-    		 if($scope.gr_config.gr_ip_type == 'ipv6'){
-    			 for(var role in $scope.gr_IP){
-    				 for(var vm in $scope.gr_IP[role]){
-        				 if($scope.gr_IP[role][vm].length != $scope.grTrafficTypeMap[$scope.gr_config.gr_traffic]){
-        					 $scope.invalideIP = true;
-        					 return;
-        				 }
-        			 }
+    		 if($scope.gr_config.pri.environment == "KVM" && $scope.gr_config.sec.environment == "KVM"){
+    			 if($scope.gr_config.gr_traffic && $scope.gr_config.gr_ip_type){
+    				 $scope.gr_ip_setup('pri');
+    				 $scope.gr_ip_setup('sec');
+    			 }
+    			 $log.info($scope.gr_IP);
+    			 
+    			 if($scope.gr_config.gr_ip_type == 'ipv6'){
+    				 for(var role in $scope.gr_IP){
+    					 for(var vm in $scope.gr_IP[role]){
+    						 if($scope.gr_IP[role][vm].length != $scope.grTrafficTypeMap[$scope.gr_config.gr_traffic]){
+    							 $scope.invalideIP = true;
+    							 return;
+    						 }
+    					 }
+    				 }
+    			 }else{
+    				 $scope.invalideIP = false;
     			 }
     		 }else{
-    			 $scope.invalideIP = false;
+    			 $scope.gr_ip_setup('pri');
+				 $scope.gr_ip_setup('sec');
     		 }
     	 };
     	 $scope.getMinTraffic = function(){
-    		 var priNic = $scope.gr_config.pri.vm_config.oam.nic.length;
-    		 var secNic = $scope.gr_config.sec.vm_config.oam.nic.length;
-    		 var minNic = priNic<secNic?priNic:secNic;
-    		 $scope.gr_traffic_types = [minNic];
-    		 for(var i=0;i<minNic;i++){
-    			 $scope.gr_traffic_types[i] = $scope.all_gr_traffic_types[i];
+    		 if($scope.gr_config.pri.environment == "KVM" && $scope.gr_config.sec.environment == "KVM"){
+    			 var priNic = $scope.gr_config.pri.vm_config.oam.nic.length;
+    			 var secNic = $scope.gr_config.sec.vm_config.oam.nic.length;
+    			 var minNic = priNic<secNic?priNic:secNic;
+    			 $scope.gr_traffic_types = [minNic];
+    			 for(var i=0;i<minNic;i++){
+    				 $scope.gr_traffic_types[i] = $scope.all_gr_traffic_types[i];
+    			 }
     		 }
+    		 $scope.gr_ip_changed();
     	 }
     	 $scope.installGR = function() {
     		 if($scope.COMStack.length > 0){
