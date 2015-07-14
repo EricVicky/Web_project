09fefe0e4b9d9d71e55a8fbe03478937b3b2a526
Eric Wang <Eric.B.Wang@alcatel-lucent.com>
2015-07-14 12:37:48 +0800

    Modify the upgrade log

--- comoam-web/src/main/java/com/alu/omc/oam/log/LogParserFactory.java
+++ comoam-web/src/main/java/com/alu/omc/oam/log/LogParserFactory.java
@@ -151,8 +151,8 @@ public class LogParserFactory
         Map<String, String> dict = new LinkedHashMap<String, String>();
         dict.put("TASK\\:\\s\\[Reboot\\sserver\\]", "Finished");
         dict.put("TASK\\:\\s\\[restore\\_data\\s\\|\\srestore\\sdata\\]", "Data Restore");
-        dict.put("TASK\\:\\s\\[run\\spost\\sreplace\\sscript\\,\\smay\\stake\\saround\\s20\\sminutes\\]", "Post Image Replacement");
-        dict.put("TASK\\:\\s\\[vnf\\_prepare\\_vms\\s\\|\\sget\\sauthorized\\skey\\]","Prepare Virtual Machines");
+        dict.put("TASK\\:\\s\\[vnf\\_create\\_vms\\s\\|\\scopy\\sqcow2\\sdisk\\simage\\]", "Post Image Replacement");
+        dict.put("TASK\\:\\s\\[vnf\\_prepare\\_vms\\s\\|\\screate\\sdata\\sdirectory\\sfor\\svirtual\\smachine\\]","Prepare Virtual Machines");
         dict.put("TASK\\:\\s\\[backup\\_data\\s\\|\\screate\\slocal\\sbackup\\sdirectory\\]", "Data Backup");
         dict.put("PLAY\\s\\[stop\\sCOM\\]", "Start");
         return new LogParser(dict);
