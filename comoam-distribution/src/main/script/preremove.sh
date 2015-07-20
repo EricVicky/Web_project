e73702b9d3a7fd5cc36e8a24452230d9c35543a7
Jeff Hu <Jeff.hu1@alcatel-lucent.com>
2015-07-20 17:51:38 +0800

    backup and restore certification

--- comoam-distribution/src/main/script/preremove.sh
+++ comoam-distribution/src/main/script/preremove.sh
@@ -6,5 +6,7 @@ if [ ! -z "$PRO_COMOAM" ]; then
     sleep 5 
 fi
 rm -f /opt/PlexView/ELCM/datasource.tar.gz
+rm -f /opt/PlexView/ELCM/cert.tar.gz
 echo "backup data"
 tar -czvf /opt/PlexView/ELCM/datasource.tar.gz  /opt/PlexView/ELCM/datasource/*
+tar -czvf /opt/PlexView/ELCM/cert.tar.gz  /opt/PlexView/ELCM/ELCM-playbook/crt/*
