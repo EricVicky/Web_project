7e320400ead7534c9eb8a9f971ea2937a6d68348
Eric Wang <Eric.B.Wang@alcatel-lucent.com>
2015-07-25 10:57:35 +0800

    modify

--- comoam-web/src/main/webapp/app/controllers/optionalVNF/os/upgradeController.js
+++ comoam-web/src/main/webapp/app/controllers/optionalVNF/os/upgradeController.js
@@ -1,6 +1,25 @@
-angular.module('os').controller('ovmupgradectr', function($scope, $filter,  $log, KVMService,  
+angular.module('os').controller('ovmupgradectr', function($scope, $filter,  $log, OSService,  
 		monitorService, DashboardService, $dialogs, $state) {
 	
 
 	
+}).controller('ovmqosacupgradectr', function($scope, $filter,  $log, OSService,  
+		monitorService, DashboardService, $dialogs, $state) {
+    
+    OSService.getComInstance().then( function(data) {
+		$log.info(data);
+		$scope.comInstance = data;
+		$scope.oscomInstance = [];
+		for(var ci=0;ci<$scope.comInstance.length;ci++){
+			if(JSON3.parse($scope.comInstance[ci].comConfig).environment ==  "OPENSTACK"){
+				if(JSON3.parse($scope.comInstance[ci].comConfig).comType == "QOSAC"){
+					$scope.oscomInstance.push($scope.comInstance[ci]);
+				}
+			}
+		}
+    });
+	
+    $scope.showInstance = function(){
+    	$scope.installConfig = JSON3.parse($scope.com_instance.comConfig);
+    };
 });
