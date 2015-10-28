1279e3939a407894b2fbff06069517f0582a6dec
Jeff Hu <jeff.hu1@alcatel-lucent.com>
2015-10-28 11:37:49 +0800

    add locked user check

--- comoam-web/src/main/java/com/alu/omc/oam/authorization/AuthController.java
+++ comoam-web/src/main/java/com/alu/omc/oam/authorization/AuthController.java
@@ -1,5 +1,8 @@
 package com.alu.omc.oam.authorization;
 
+import java.io.IOException;
+
+import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
 
 import net.sf.jpam.Pam;
@@ -14,11 +17,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.alu.omc.oam.util.CommandProtype;
+import com.alu.omc.oam.util.CommandResult;
 import com.alu.omc.oam.util.EncryptUtils;
+import com.alu.omc.oam.util.ICommandExec;
 
 @Controller("authController")
 public class AuthController {
-
+    final String checkLockedUser = "checkLockedUser";
+    @Resource
+    private  CommandProtype commandProtype;
+    
 	@RequestMapping(value="/login", method=RequestMethod.POST)
 	public @ResponseBody
     UserAccount login(@RequestBody final UserAccount user, HttpSession session) {
@@ -40,24 +49,33 @@ public class AuthController {
                 log.info("Deny root user login");
                 return user;
         }
-        
         Pam pam = new Pam();
         PamReturnValue ret = pam.authenticate(user.getUsername(), user.getPassword());
         log.info("verifyMsg" + ret.toString());
-        if ( ret == PamReturnValue.PAM_SUCCESS) {
-                user.setReason("");
-                user.setPassword("");
-                String token = EncryptUtils.encryptMD5(user.getUsername() + "" + user.getPassword());
-                user.setToken(token);
-                user.setPassword("");
-                user.setReason(null);
-                session.setAttribute("token", token);
-                log.info("user: " + user.getUsername() + "  -- login successful");
-                return user;
-        } else {
-                user.setReason("invalid user or password");
+        if (ret == PamReturnValue.PAM_SUCCESS)
+        {
+            if (isLocked(user.getUsername()))
+            {
+                user.setReason("user is locked");
                 user.setPassword("");
                 return user;
+            }
+            user.setReason("");
+            user.setPassword("");
+            String token = EncryptUtils.encryptMD5(user.getUsername() + ""
+                    + user.getPassword());
+            user.setToken(token);
+            user.setPassword("");
+            user.setReason(null);
+            session.setAttribute("token", token);
+            log.info("user: " + user.getUsername() + "  -- login successful");
+            return user;
+        }
+        else
+        {
+            user.setReason("invalid user or password");
+            user.setPassword("");
+            return user;
         }
 }
 	@RequestMapping(value="/logout",  method=RequestMethod.DELETE)
@@ -65,7 +83,25 @@ public class AuthController {
     void logout( HttpSession session) {
 	    session.invalidate();
 	}
-
+	
+	boolean isLocked(String user){
+	    boolean locked = false;
+	    ICommandExec comamnda = commandProtype.create("checkLockedUser.sh " + user);
+	    
+	    try
+        {
+	        CommandResult res = comamnda.execute();
+	        if(res.getExitValue() != 1){
+	            locked = true;
+	        }
+        }
+        catch (IOException | InterruptedException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+	    return locked;
+	}
 
 	private static Logger log = LoggerFactory.getLogger(AuthController.class);
 
