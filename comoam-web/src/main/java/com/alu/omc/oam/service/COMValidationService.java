2381d61cbd761a4e40e5e5530769b19931d97380
Jeff Hu <jeff.hu1@alcatel-lucent.com>
2015-11-06 09:22:55 +0800

    add check COM validation

--- comoam-web/src/main/java/com/alu/omc/oam/service/COMValidationService.java
+++ comoam-web/src/main/java/com/alu/omc/oam/service/COMValidationService.java
@@ -1,38 +1,25 @@
 package com.alu.omc.oam.service;
-import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.PipedInputStream;
-import java.io.PipedOutputStream;
-import java.util.Set;
 
-import com.alu.omc.oam.ansible.validation.ValidationResult;
+import org.apache.commons.lang3.SystemUtils;
+
 import com.jcraft.jsch.Channel;
-import com.jcraft.jsch.ChannelExec;
-import com.jcraft.jsch.ChannelShell;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
-import com.jcraft.jsch.UserInfo;
 
 public class COMValidationService {
 	
-	private String username;
-	private String oamip;
-	private String password;
+	private String username = "axadmin";
+	private String oamip ;
+	private String password = "newsys";
 	
 	private Session getSession(String username, String oamip, String password){
         JSch shell = new JSch();
-//        String privateKey = "/root/.ssh/id_rsa";
-//		
-//		java.util.Properties config = new java.util.Properties();
-//		config.put("StrictHostKeyChecking", "no");
-		
-		JSch ssh = new JSch();
         Session session = null;
         try {
-//        	ssh.addIdentity(privateKey);
 			session = shell.getSession(username, oamip, 22);
 	        session.setPassword(password);
 	        session.setConfig("StrictHostKeyChecking", "no");
@@ -43,6 +30,26 @@ public class COMValidationService {
 			e.printStackTrace();
 		}  
         return session;
+	}
+	
+	private Session getSession(String ip){
+        String privateKey = "/root/.ssh/id_rsa";
+        
+        java.util.Properties config = new java.util.Properties();
+        config.put("StrictHostKeyChecking", "no");
+        
+        JSch ssh = new JSch();
+        try
+        {
+            ssh.addIdentity(privateKey);
+            Session session = ssh.getSession("root", ip, 22);
+            return session;
+        }
+        catch (JSchException e)
+        {
+            e.printStackTrace();
+        }
+        return null;
 	}
 	
     private Channel getChannel(Session session){
@@ -59,7 +66,12 @@ public class COMValidationService {
     }
     
     public String excuteShell(String command){
-    	Session session = getSession(this.username, this.oamip, this.password);
+        Session session = null;
+        if(SystemUtils.IS_OS_WINDOWS){
+            session = getSession(this.username, this.oamip, this.password);
+        }else{
+            session = getSession(command);
+        }
     	Channel channel = getChannel(session);
 		String finalCommand = command + " \n";
 		String string = null;
@@ -108,20 +120,18 @@ public class COMValidationService {
     	this.password = password;
     }
     
-    public boolean checkCOM(){
+    public boolean checkIfCOMProcessUp(String ip){
     	String resutlCheckCOM = excuteShell("CheckCOM");
     	System.out.println("" + resutlCheckCOM);
-    	String resultCheckInstallLog = excuteShell("/install/scripts/checkInstallLog.sh");
-    	if(resutlCheckCOM.contains("Number of stopped process(es) : 0") && !resultCheckInstallLog.contains("-E-")){
+    	//String resultCheckInstallLog = excuteShell("/install/scripts/checkInstallLog.sh");
+    	if(resutlCheckCOM.contains("Number of stopped process(es) : 0") ){
     		return true;
-    	}
-    	
-    	else {
+    	} else {
     		return false;
     	}
     }
     
-	public static void main(String[] args) {
+/*	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		COMValidationService sessionToCOM = new COMValidationService();
 		sessionToCOM.setoamip("10.223.1.85");
@@ -130,6 +140,6 @@ public class COMValidationService {
         boolean syl = sessionToCOM.checkCOM();
 		System.out.println("" + syl);
 
-	}
+	}*/
 
 }
